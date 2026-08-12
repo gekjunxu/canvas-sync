0b26a245013bc12049f88558e343c5838646f126
gekjunxu <E1122727@u.nus.edu>
2026-08-12 14:06:14 +0800

    respect theme colors in course tree

--- lib/tree_model.cc
+++ lib/tree_model.cc
@@ -275,12 +275,13 @@ QVariant TreeModel::data(const QModelIndex &index, int role) const
   }
 
   if (role == Qt::ForegroundRole) {
+    const QPalette palette = qApp->palette();
     if (index.column() == TreeIndex::LOCAL_DIR) {
-      return QVariant(QColor(item->isChecked() && item->has_local_dir()
-                                 ? Qt::black
-                                 : Qt::lightGray));
+      return QVariant(item->isChecked() && item->has_local_dir()
+                          ? palette.color(QPalette::Text)
+                          : palette.color(QPalette::Disabled, QPalette::Text));
     }
-    return QVariant(QColor(Qt::black));
+    return QVariant(palette.color(QPalette::Text));
   }
 
   return QVariant();
