3ccce2ad20ce7805e2174ec55dd80c39bc6a3330
gekjunxu <E1122727@u.nus.edu>
2026-08-12 14:16:28 +0800

    fix light theme button text

--- app.cc
+++ app.cc
@@ -380,6 +380,7 @@ void App::apply_theme(const QString &theme)
     palette.setColor(QPalette::Text, Qt::white);
     palette.setColor(QPalette::Button, QColor("#353535"));
     palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor("#a8a8a8"));
     palette.setColor(QPalette::Highlight, QColor("#3d6f9e"));
     palette.setColor(QPalette::HighlightedText, Qt::white);
     palette.setColor(QPalette::PlaceholderText, QColor("#b0b0b0"));
@@ -391,6 +392,8 @@ void App::apply_theme(const QString &theme)
     palette.setColor(QPalette::Text, QColor("#202020"));
     palette.setColor(QPalette::WindowText, QColor("#202020"));
     palette.setColor(QPalette::ButtonText, QColor("#202020"));
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor("#707070"));
+    palette.setColor(QPalette::Disabled, QPalette::Text, QColor("#707070"));
     palette.setColor(QPalette::HighlightedText, QColor("#202020"));
   }
   QApplication::setPalette(palette);
